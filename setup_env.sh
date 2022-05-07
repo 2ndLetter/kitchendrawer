98924bc65e8936df2215de7255737aa703cb3ec8
Brian <b.matthew.chadwick@gmail.com>
2022-05-07 23:45:21 +0000

    updated script

--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Prerequisites
+sudo yum install git -y
+
 # Setup new ssh key for GitHub
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
 
-echo "Script finished!"
+cat ~/.ssh/id_rsa.pub
