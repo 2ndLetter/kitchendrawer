60bb1055085e1ed39068bbbc49d5d9e788cc0bcd
Brian Chadwick <b.matthew.chadwick@gmail.com>
2022-05-08 00:54:11 +0000

    updating script

--- setup_env.sh
+++ setup_env.sh
@@ -18,4 +18,10 @@ ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
 # Cat public key for adding to GitHub
 cat ~/.ssh/id_rsa.pub
 
-
+# Install Python 3.x
+wget https://www.python.org/ftp/python/3.10.4/Python-3.10.4.tgz
+tar xzf Python-3.10.4.tgz
+cd Python-3.10.4
+sudo ./configure --enable-optimizations 
+sudo make altinstall
+sudo rm Python-3.10.4.tgz
