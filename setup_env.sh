9036cb86fcc46e2e524dacfb9d92e2d65f98ecef
Brian Chadwick <b.matthew.chadwick@gmail.com>
2022-05-08 00:08:01 +0000

    updated script

--- setup_env.sh
+++ setup_env.sh
@@ -3,7 +3,15 @@
 # Prerequisites
 sudo yum install git -y
 
+read -p "Enter email: " email
+read -p "Enter name: " name
+
+git config --global user.email "$email"
+git config --global user.name "$name"
+
 # Setup new ssh key for GitHub
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
 
 cat ~/.ssh/id_rsa.pub
+
+
